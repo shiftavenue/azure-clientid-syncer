4a67a0dc073dd8ad570ed84d24ebe79eed19ebb9
Felix Wenzel <felix@Felixs-Laptop.fritz.box>
2024-01-09 14:57:20 +0100

    add helm pipeline and setup description

--- pkg/util/sa_info.go
+++ pkg/util/sa_info.go
@@ -3,9 +3,9 @@ package util
 import "os"
 
 func GetNamespace() string {
-	ns, found := os.LookupEnv("POD_NAMESPACE")
+	ns, found := os.LookupEnv("SA_NAMESPACE")
 	if !found {
-		return "azure-clientid-syncer"
+		return "azure-clientid-syncer-system"
 	}
 	return ns
 }
