c86d83730b85e96adb62838661392e2b7118fff6
Rene Schach <rene.schach@shiftavenue.com>
2024-02-15 18:42:50 +0100

    refactor(gcp): Update GCP querying to use Asset Inventory API (#21)
    * refactor(gcp): Update GCP querying to use Asset Inventory API
    
    * add RG param to KV in e2e-test to assure it can be found every time
    
    ---------
    
    Co-authored-by: Felix <felixwenzel0@gmail.com>

--- pkg/provider/const.go
+++ pkg/provider/const.go
@@ -8,6 +8,8 @@ const (
 
 	// gcpRoleName represents the role associated with service accounts in GCP allowing them to use workload identity
 	gcpRoleName = "roles/iam.workloadIdentityUser"
+	// gcpResourceAssetType represents the Asset Inventory resource type for GCP service accounts
+	gcpResourceAssetType = "iam.googleapis.com/ServiceAccount"
 	// gcpServiceAccountAnnotation represents the GCP service account name to be used with the Kubernetes service account
 	gcpServiceAccountAnnotation = "iam.gke.io/gcp-service-account"
 )
